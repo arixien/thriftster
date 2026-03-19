f51b8c9361716db205d6df42e81f3ad8c4744b73
Quervie Manrique <manriquequervie@gmail.com>
2026-03-19 12:15:07 +0800

    feat: add login,php and register.php

---
+++ app/Views/login.php
@@ -0,0 +1,7 @@
+<form method="post">
+    <?= isset($error) ? $error : '' ?>
+    Username: <input type="text" name="username"><br>
+    Password: <input type="password" name="password"><br>
+    <button type="submit">Login</button>
+</form>
+<a href="/auth/register">Don't have an account? Register</a>
---
+++ app/Views/register.php
@@ -0,0 +1,8 @@
+<form method="post">
+    <?= isset($validation) ? $validation->listErrors() : '' ?>
+    Username: <input type="text" name="username"><br>
+    Email: <input type="email" name="email"><br>
+    Password: <input type="password" name="password"><br>
+    <button type="submit">Register</button>
+</form>
+<a href="/auth/login">Already have an account? Login</a>
