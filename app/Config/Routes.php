375a20111cc955f0e8c838430211274974ac3a7f
Quervie Manrique <manriquequervie@gmail.com>
2026-03-19 12:15:20 +0800

    refactor: route.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,7 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Storefront::index');
 $routes->get('/products_catalog_view', 'ProductsCatalog::index');
+$routes->get('/auth/register', 'Auth::register');
+$routes->match(['get','post'], '/auth/login', 'Auth::login');
+$routes->get('/auth/logout', 'Auth::logout');
+$routes->get('/dashboard', 'Dashboard::index');
