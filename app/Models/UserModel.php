30e04d57f2336e0a37c0a7c350b60940e151b4c0
mm-gonzales <michaela.v.gonzales@gmail.com>
2026-03-24 10:00:02 +0800

    feat: add 'role' on allowed fields

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,6 +6,6 @@ class UserModel extends Model
 {
     protected $table = 'users';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['username', 'email', 'password'];
+    protected $allowedFields = ['username', 'email', 'password', 'role'];
 }
 ?>
