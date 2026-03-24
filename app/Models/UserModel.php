cc377c5eb2402359452200ffbf3f973fb74378c7
mm-gonzales <michaela.v.gonzales@gmail.com>
2026-03-24 10:12:18 +0800

    refactor: format allowedFields array for better readability

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,7 +6,12 @@ class UserModel extends Model
 {
     protected $table = 'users';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['username', 'email', 'password', 'role'];
+    protected $allowedFields = [
+        'username', 
+        'email', 
+        'password', '
+        role'
+    ];
 
     protected $useTimestamps = true;
     protected $createdField  = 'created_at';
