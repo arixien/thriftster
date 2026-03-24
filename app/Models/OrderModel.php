970b99bbcc58b81e966b717c78d7ac4eb26a9b5e
mm-gonzales <michaela.v.gonzales@gmail.com>
2026-03-24 10:18:27 +0800

    feat: implement OrderModel

---
+++ app/Models/OrderModel.php
@@ -0,0 +1,35 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class OrderModel extends Model
+{
+    protected $table = 'orders';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+        'order_number',
+        'buyer_id',
+        'status',
+        'subtotal',
+        'delivery_fee',
+        'total_amount',
+        'delivery_method',
+        'payment_method',
+        'estimated_delivery_at',
+        'paid_at',
+        'shipping_first_name',
+        'shipping_last_name',
+        'shipping_email',
+        'shipping_phone',
+        'shipping_address',
+        'shipping_barangay',
+        'shipping_city',
+        'shipping_region',
+        'shipping_postal_code',
+    ];
+
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+}
+?>
